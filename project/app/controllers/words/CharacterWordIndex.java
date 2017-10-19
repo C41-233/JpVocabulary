46741ede6460b3ea73d4dab819d1909a60f9c2d8
C41-233 <c41hahaha@163.com>
2017-10-19 23:41:55 +0800

    合并多个对话框

--- project/app/controllers/words/CharacterWordIndex.java
+++ project/app/controllers/words/CharacterWordIndex.java
@@ -1,6 +1,7 @@
 package controllers.words;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import base.utility.Strings;
@@ -8,6 +9,7 @@ import base.utility.linq.Linq;
 import core.controller.HtmlControllerBase;
 import core.controller.validation.annotation.Required;
 import logic.characters.CharacterIndexManager;
+import logic.pinyins.Pinyins;
 
 public class CharacterWordIndex extends HtmlControllerBase{
 
@@ -39,7 +41,55 @@ public class CharacterWordIndex extends HtmlControllerBase{
 		
 		renderArgs.put("indexes", indexesVO);
 		
-		render("words/word-index");
+		List<CharacterWordSyllableGroupVO> syllableGroupsVO = new ArrayList<>();
+		for(int i=1; i<=4 ;i++) {
+			String groupName = index + i;
+			CharacterWordSyllableGroupVO syllableGroupVO = new CharacterWordSyllableGroupVO();
+			syllableGroupVO.name = Pinyins.toPinyin(groupName);
+			
+			{
+				CharacterWordGroupVO groupVO = new CharacterWordGroupVO();
+				syllableGroupVO.characters.add(groupVO);
+				
+				groupVO.name = "啊";
+				{
+					WordVO wordVO = new WordVO();
+					wordVO.value = "愛野";
+					wordVO.syllable = "あいの";
+					wordVO.meanings = (Arrays.asList("干","打（哈哈）"));
+					wordVO.types = Arrays.asList("名词", "自动词");
+					groupVO.words.add(wordVO);
+				}
+				{
+					WordVO wordVO = new WordVO();
+					wordVO.value = "愛野";
+					wordVO.syllable = "あいの";
+					wordVO.meanings = (Arrays.asList("干"));
+					wordVO.types = Arrays.asList("名词", "自动词", "副词");
+					groupVO.words.add(wordVO);
+				}
+			}
+			
+			{
+				CharacterWordGroupVO groupVO = new CharacterWordGroupVO();
+				syllableGroupVO.characters.add(groupVO);
+				
+				groupVO.name = "的";
+				{
+					WordVO wordVO = new WordVO();
+					wordVO.value = "愛野";
+					wordVO.syllable = "あいの";
+					wordVO.meanings = (Arrays.asList("干","打（哈哈）"));
+					wordVO.types = Arrays.asList("接头词");
+					groupVO.words.add(wordVO);
+				}
+			}
+
+			syllableGroupsVO.add(syllableGroupVO);
+		}
+		
+		renderArgs.put("syllables", syllableGroupsVO);
+		render("words/word-character-index");
 	}
 
 	private static class ArgVO{
@@ -51,6 +101,24 @@ public class CharacterWordIndex extends HtmlControllerBase{
 		String name;
 		List<String> items = new ArrayList<>();
 		int seq;
+	}
+	
+	
+	private static class CharacterWordSyllableGroupVO{
+		String name;
+		List<CharacterWordGroupVO> characters = new ArrayList<>();
+	}
+	
+	private static class CharacterWordGroupVO{
+		String name;
+		List<WordVO> words = new ArrayList<>();
+	}
+	
+	private static class WordVO{
+		String value;
+		String syllable;
+		List<String> meanings = new ArrayList<>();
+		List<String> types = new ArrayList<>();
 	}
 	
 }
--- project/public/javascripts/jpn-common.js
+++ project/public/javascripts/jpn-common.js
@@ -37,9 +37,24 @@ global.Validate = {
 
 })();
 
+//dump
+$(function(){
+	$("#btn-mysqldump").click(function(){
+		Dialog.wait(function(handle){
+			Action.post("/action/dump", {}, {
+				success: function(){
+					alert("备份完成")
+				},
+				complete: function(){
+					handle.close()
+				}	
+			})
+		})
+	})
+})
 
 $(function(){
-
+	//左侧列表
 	$("#left-list").accordion({
 		header: ".left-list-header",
 		heightStyle: "content",
@@ -47,5 +62,95 @@ $(function(){
 		collapsible: false,
 		active: $("#left-list .left-list-header-active").data("seq"),
 	}).show()
+})
+	
+//添加汉字
+$(function(){
+	function actionCreateCharacter(){
+		if($("#dialog-import-character").validate("hasError")){
+			return
+		}
+		
+		var jp = $("#import-character-input-jp").val()
+		var cn = $("#import-character-input-cn").val()
+		
+		var pinyins = Validate.parsePinyins($("#import-character-input-pinyin").val())
+		
+		Action.post("/characters/action/create", {jp: jp, cn: cn, pinyins: pinyins}, function(data){
+			location.href = data.href
+		})
+	}
+	
+	var dialog = $("#dialog-import-character").dialog({
+		autoOpen: false,
+		modal: true,
+		resizable: false,
+		draggable: false,
+		width:	260,
+		buttons: {
+			"创建": actionCreateCharacter,
+			"取消": function(){
+				$(this).dialog("close")
+			}
+		},
+		close: function(){
+			$(this).validate("clear")
+		}
+	})
+	$("#btn-import-character").click(function(){
+		$("#dialog-import-character").dialog("open")
+	})
+	dialog.dialog("widget").find(".ui-dialog-buttonset button").addClass("btn btn-default btn-xs")
+	
+	$("#import-character-input-jp, #import-character-input-cn, #import-character-input-pinyin").keydown(function(e){
+		if(e.keyCode == VK.ENTER && e.ctrlKey){
+			actionCreateCharacter()
+		}	
+	})
+	
+	$("#dialog-import-character").validate({
+		"#import-character-input-jp": Validate.isValidJpCharacter,
+		"#import-character-input-cn": Validate.isValidCnCharacter,
+		"#import-character-input-pinyin": function(val){
+			try{
+				var tokens = Validate.parsePinyins(val)
+				if(tokens.length == 0){
+					return false
+				}
+				return true
+			} catch(e){
+				return e
+			}
+		}
+	})
+	
+})
 
-});
+//添加基本词
+$(function(){
+	function actionCreateNotional(){
+	
+	}
+
+	var dialog = $("#dialog-import-notional-word").dialog({
+		autoOpen: false,
+		modal: true,
+		resizable: false,
+		draggable: false,
+		width:	320,
+		buttons: {
+			"创建": actionCreateNotional,
+			"取消": function(){
+				$(this).dialog("close")
+			}
+		},
+		close: function(){
+			$(this).validate("clear")
+		}
+	})
+	$("#btn-import-notional-word").click(function(){
+		$("#dialog-import-notional-word").dialog("open")
+	})
+	dialog.dialog("widget").find(".ui-dialog-buttonset button").addClass("btn btn-default btn-xs")
+	
+})
