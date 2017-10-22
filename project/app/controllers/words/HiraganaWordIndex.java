712182c513d970008913d5e3627a3cfea03ec1e3
C41-233 <c41hahaha@163.com>
2017-10-22 23:30:54 +0800

    平假名索引

--- project/app/controllers/words/HiraganaWordIndex.java
+++ project/app/controllers/words/HiraganaWordIndex.java
@@ -20,25 +20,30 @@ public final class HiraganaWordIndex extends HtmlControllerBase{
 			notFound(Strings.format("%s not found.", index));
 		}
 
+		//TODO 标签化，有三处重复了，改成一个tag
 		ArgVO arg = new ArgVO();
 		arg.index = index;
 		renderArgs.put("arg", arg);
 			
-		List<HiraganaIndexVO> indexesVO = Linq.from(IndexManager.Character.getGroups()).select((group, i)->{
-			HiraganaIndexVO vo = new HiraganaIndexVO();
-			vo.name = group.getName();
-			vo.seq = i;
-			for(String item : group.getItems()) {
-				vo.items.add(item);
-				if(item.equals(index)) {
-					arg.group = vo.name;
-				}
-			}
-			return vo;
-		}).toList();
+		List<HiraganaIndexVO> indexesVO = Linq.from(IndexManager.Hiragana.getGroups())
+				.union(IndexManager.Character.getGroups())
+				.select((group, i)->{
+					HiraganaIndexVO vo = new HiraganaIndexVO();
+					vo.name = group.getName();
+					vo.seq = i;
+					for(String item : group.getItems()) {
+						vo.items.add(item);
+						if(item.equals(index)) {
+							arg.group = vo.name;
+						}
+					}
+					return vo;
+				})
+				.toList();
 		
 		renderArgs.put("indexes", indexesVO);
-		
+
+		render("words/word-hiragana-index");
 	}
 
 	private static class ArgVO{
