1f834ac1617949ef11a3093832a30022364417a7
C41-233 <c41hahaha@163.com>
2017-11-17 22:50:03 +0800

    五十音图完善

--- project/app/controllers/Page.java
+++ project/app/controllers/Page.java
@@ -3,8 +3,12 @@ package controllers;
 import core.controller.HtmlControllerBase;
 
 public final class Page extends HtmlControllerBase{
-
-	public static void of(String file) {
+	
+	public static void _50() {
+		of("50");
+	}
+	
+	private static void of(String file) {
 		render("pages/"+file);
 	}
 	
