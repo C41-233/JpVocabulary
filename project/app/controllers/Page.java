d431ff37d804a39adada2c8e67609419fb3af00a
C41-233 <c41hahaha@163.com>
2017-11-17 23:23:09 +0800

    用言变形

--- project/app/controllers/Page.java
+++ project/app/controllers/Page.java
@@ -7,6 +7,14 @@ public final class Page extends HtmlControllerBase{
 	public static void _50() {
 		of("50");
 	}
+
+	public static void verb() {
+		of("verb");
+	}
+
+	public static void other() {
+		of("other");
+	}
 	
 	private static void of(String file) {
 		render("pages/"+file);
