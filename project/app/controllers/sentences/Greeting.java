a48a3f3a6e0360cb193fbd8cc7288ed9d50358c2
C41-233 <c41hahaha@163.com>
2018-01-11 00:00:46 +0800

    寒暄语

---
+++ project/app/controllers/sentences/Greeting.java
@@ -0,0 +1,47 @@
+package controllers.sentences;
+
+import java.io.File;
+import java.util.List;
+
+import base.utility.linq.Linq;
+import base.xml.XmlReader;
+import core.controller.HtmlControllerBase;
+
+public class Greeting extends HtmlControllerBase{
+
+	private static final File BaseFile = new File("data/greetings");
+	private static final File ContentFile = new File(BaseFile, "contents.xml");
+	
+	public static void index() {
+		List<Content> contents = readContents();
+		page(contents.get(0).name);
+	}
+	
+	public static void page(String name) {
+		List<Content> contents = readContents();
+		Content current = Linq.from(contents).findFirst(c->c.name.equals(name));
+		if(current == null) {
+			notFound();
+		}
+		renderArgs.put("current", current);
+		
+		File file = new File(BaseFile, name+".xml");
+		if(file.exists()) {
+		}
+		
+		render("sentences/greetings");
+	}
+	
+	private static List<Content> readContents() {
+		XmlReader reader = new XmlReader();
+		List<Content> contents = reader.readList(ContentFile, Content.class, "content");
+		renderArgs.put("contents", contents);
+		return contents;
+	}
+	
+	private static class Content{
+		public String name;
+		public String value;
+	}
+	
+}
